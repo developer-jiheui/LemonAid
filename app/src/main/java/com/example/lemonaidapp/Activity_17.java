2e7f5e4cc644c9f65bffea3abe90f202604bd9cf
developer-jiheui <developer.jiheuilee@gmail.com>
2020-03-26 18:35:53 -0700

    Activity 17 update

--- app/src/main/java/com/example/lemonaidapp/Activity_17.java
+++ app/src/main/java/com/example/lemonaidapp/Activity_17.java
@@ -2,13 +2,35 @@ package com.example.lemonaidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity_17 extends AppCompatActivity {
+    DatabaseHelper dbh;
+    String userMsgInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_17);
+
+        dbh = new DatabaseHelper(this);
+        final EditText patientMsg = findViewById(R.id.editComplaint);
+        Button btnPatientMsg = findViewById(R.id.btnSubmitComplaint);
+
+
+        btnPatientMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userMsgInput = patientMsg.getText().toString();
+                //put this input to the database somehow
+                Intent i = new Intent(Activity_17.this,Activity_18.class);
+                startActivity(i);
+            }
+        });
     }
 }
